6ad25f4bb6671fdb4c3f9191d469c69a304cec93
Valerius Mattfeld <mail@valerius.me>
2024-07-14 10:48:47 +0200

    feat: multiple files, more caching, refactoring

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pathlib import Path
+from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import PlainTextResponse
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
-from fastapi_utilities import repeat_every
 from fastapi_cache.decorator import cache
-
+from fastapi_utilities import repeat_every
 from git import Repo
 
 REPO_URL = "https://github.com/github/gitignore.git"
@@ -31,44 +31,75 @@ async def update_repository():
 @cache(expire=300)
 async def root():
     files = await get_file_list()
-    return {"available": f.replace(".gitignore", "") for f in files}
+    return {"available": [f.replace(".gitignore", "") for f in files]}
 
 
-@app.get('/{lang}')
+@app.get('/{languages}')
 @cache(expire=300)
-async def get_language_ignore_file(lang: str):
-    lang = lang.lower()
+async def get_language_ignore_file(languages: str):
+    langs = languages.lower().strip()
+    if ',' in langs:
+        langs = langs.split(',')
+    else:
+        langs = [langs]
+
+    # filter out empty strings
+    langs = [lang for lang in langs if lang]
+    if not langs:
+        raise HTTPException(status_code=400, detail="No languages provided")
+
     repo_path = Path(REPO_DIR)
     file_list = await get_file_list()
 
-    # if lang does not include .gitignore, add it
-    if '.gitignore' not in lang:
-        lang += '.gitignore'
-
-    # if lang is not in the list of files, return an error
-    if lang not in file_list:
-        return {'status': 'error. Could not find language file'}
-
-    # open the file and return its contents
-    with open(repo_path / lang, 'r') as file:
-        # return the contents of the file as plain text
-        contents = file.read()
-
-    return PlainTextResponse(contents, media_type='text/plain')
+    contents = []
+    for lang in langs:
+        contents.append(await get_file_contents(lang, repo_path, file_list))
+    joined_contents = await post_process_contents(contents)
+    return PlainTextResponse(joined_contents, media_type='text/plain')
 
 
 async def get_file_list():
-    repo_path = Path(REPO_DIR)
-    if not repo_path.exists():
-        try:
-            await update_repository()
-        except Exception as e:
-            print(e)
-            return {'status': 'error'}
-        if not repo_path.exists():
-            return {'status': 'error. Could not find repository'}
-
+    repo_path = await check_repository()
+    if repo_path is None:
+        raise HTTPException(status_code=500, detail="Could not find repository")
     # list all files in the repository
     file_list = list(repo_path.glob('*.gitignore'))
     # convert to lowercase
     return [file.name.lower() for file in file_list]
+
+
+async def check_repository() -> Path | None:
+    repo_path = Path(REPO_DIR)
+    if not repo_path.exists():
+        try:
+            await update_repository()
+            return repo_path
+        except Exception as e:
+            print(e)
+            return None
+    return repo_path
+
+
+@cache(expire=300)
+async def get_file_contents(lang: str, repo_path: Path, file_list: List[str]) -> str:
+    if '.gitignore' not in lang:
+        lang += '.gitignore'
+    if lang not in file_list:
+        raise HTTPException(status_code=404, detail="Could not find language file")
+
+    with open(repo_path / lang.capitalize(), 'r') as file:
+        content = file.read()
+        return f'# ------------- {lang.capitalize()} -------------\n\n' + content
+
+
+async def post_process_contents(contents: List[str]) -> str:
+    joined_contents = '\n'.join(contents)
+    lines = joined_contents.splitlines()
+    occurred_lines = {}
+
+    for i, line in enumerate(lines):
+        if line in occurred_lines:
+            lines[i] = ""
+        else:
+            occurred_lines[line] = True
+    return '\n'.join(lines)
